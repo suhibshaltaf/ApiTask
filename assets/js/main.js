574015e2af2c54ed3bdf1851e1a676c4807c1812
sohaibshaltaf <sohaibshaltafaltrefe@gmail.com>
2026-01-02 22:21:42 +0300

    category section and js

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+const getcategorys = async () => {  
+
+    const response = await axios.get('https://dummyjson.com/products/category-list');
+
+    return response.data;
+}
+const displaydatacategory=async()=>{
+  try{
+
+    const result=await getcategorys();
+
+    const categorys=result.map((category)=>{
+    return`
+    
+  <div class="col-md-3 p-2 ">
+    <div class="card category-card text-center w-100">
+        <div class="card-body ">
+            
+            <h5 class="card-title mb-4 text-info fw-semibold">
+                ${category}
+            </h5>
+
+            <a href=""
+               class="btn btn-outline-info rounded-pill mt-auto">
+                Show Products
+            </a>
+
+        </div>
+    </div>
+</div>
+
+
+            `  
+    }).join(' ');
+ 
+    document.querySelector(".container .row").innerHTML=categorys;
+}
+catch(error){
+    document.querySelector(".errorClass").classList.remove("d-none");
+                document.querySelector(".errorClass").textContent=error.message;
+
+
+}
+finally{
+    document.querySelector(".loader").classList.add("d-none");
+}
+}
+displaydatacategory();
